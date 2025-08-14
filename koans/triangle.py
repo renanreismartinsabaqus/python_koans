3c3cf12e76fddacaa49823310c5a71dd71433fd4
Renan Reis Martins de Paula <rreis@abaqus.cl>
2025-08-13 21:22:57 -0400

    + exercises

--- koans/triangle.py
+++ koans/triangle.py
@@ -16,9 +16,23 @@
 # and
 #   about_triangle_project_2.py
 #
+
+def count_equals(elements, count):
+    if (elements == []):
+         return count
+    #reduce?
+    return count_equals(elements[1:], count + elements[1:].count(elements[0]))
+
 def triangle(a, b, c):
-    # DELETE 'PASS' AND WRITE THIS CODE
-    pass
+    c = count_equals([a, b, c], 0) 
+    print(c)
+    match c:
+        case 0:
+            return "scalene"
+        case 1:
+            return "isosceles"
+        case 3:
+            return "equilateral"
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError(Exception):
